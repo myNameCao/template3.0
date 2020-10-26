f98e8aaaddf498d04d7597507f505733aa10f114
hefei <hefei.cao@xiaoyangedu.com>
2020-10-26 13:48:51 +0800

    axios

--- template/README.md
+++ template/README.md
@@ -1,21 +1,26 @@
 # template
 
 ## Project setup
+
 ```
 yarn install
+
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 yarn build
 ```
 
 ### Lints and fixes files
+
 ```
 
 yarn lint
@@ -23,4 +28,27 @@ yarn lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### axios
+
+方法一（异步回调）
+
+```js
+ajax({
+  url: 'AccountsInfo',
+  success(res) {
+    // todo something
+  }
+})
+```
+
+方法二（async）
+
+```js
+;async () => {
+  let { data } = await ajax({ url: 'AccountsInfo' })
+  // todo something
+}
+```
--- template/src/utils/axios/index.js
+++ template/src/utils/axios/index.js
@@ -5,9 +5,10 @@ import {
   responseSuccess,
   responseFail
 } from '@conf'
+
 // 站点统一的配置
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_HOST,
+  baseURL: process.env.VUE_APP_HOST,
   timeout: 15000,
   contentType: 'application/x-www-form-urlencoded',
   complete: function(xhr, status) {
@@ -18,22 +19,26 @@ const instance = axios.create({
 })
 // 注入请求拦截
 instance.interceptors.request.use(requestSuccess, requestFail)
+
 // 注入响应拦截
 instance.interceptors.response.use(responseSuccess, responseFail)
-// 过程组装请求
 
-export const ajax = P => {
-  return instance({
-    method: P.type || 'get',
-    url: P.url,
-    params: P.params ? P.params : null,
-    data: P.data ? P.data : null
+// 过程组装请求
+export const ajax = async P => {
+  return new Promise(resolve => {
+    instance({
+      method: P.type || 'get',
+      url: P.url,
+      params: P.params ? P.params : null,
+      data: P.data ? P.data : null
+    })
+      .then(res => {
+        //TODO: 后端数据在这添加 预编译
+        resolve(res)
+        P.success && P.success(res)
+      })
+      .catch(err => {
+        P.error && P.error(err)
+      })
   })
-    .then(res => {
-      // 状态码的处理
-      P.success && P.success(res.data)
-    })
-    .catch(function(err) {
-      P.error && P.error(err)
-    })
 } //web请求: ajax封装
