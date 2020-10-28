79ee671f01f94687086e70c8f2e2282045092293
hefei <hefei.cao@xiaoyangedu.com>
2020-10-28 18:42:31 +0800

    add vant

--- mobile/index.js
+++ mobile/index.js
@@ -1,9 +1,9 @@
 module.exports = (api, options = {}, rootOptions = {}) => {
-  console.log(options)
   api.extendPackage({
     dependencies: {
       vant: '^2.10.11'
     }
   })
+  api.injectImports(api.entryFile, `import './iview'`)
   api.render('./template')
 }
