7eebfe7a7054af106b28bcffe951ecb3c3ef6fc7
hefei <hefei.cao@xiaoyangedu.com>
2020-11-02 15:16:29 +0800

    add postcss-px-to-viewport

--- mobile/index.js
+++ mobile/index.js
@@ -2,8 +2,11 @@ module.exports = (api, options = {}, rootOptions = {}) => {
   api.extendPackage({
     dependencies: {
       vant: '^2.10.11'
+    },
+    devDependencies: {
+      'postcss-px-to-viewport': '^1.1.1'
     }
   })
-  api.injectImports(api.entryFile, `import './iview'`)
+  api.injectImports(api.entryFile, `import './vant'`)
   api.render('./template')
 }
--- template/src/views/common/home.vue
+++ template/src/views/common/home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     晓羊欢迎。。。。。
+
+    <div class="run"></div>
   </div>
 </template>
 
@@ -10,3 +12,14 @@
     name: 'Home'
   }
 </script>
+
+<style lang="less" scpoed>
+  .home {
+    width: 750px;
+    box-sizing: border-box;
+    .run {
+      width: 375px;
+      height: 200px;
+    }
+  }
+</style>
