502a1724f7c8b896b4ce232effbad6ad48324b98
caohefei <hefei.cao@xiaoyangedu.com>
2020-11-06 15:59:11 +0800

    rm index

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,13 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
 import routes from './routes'
 import { routerBeforeEach } from '@conf'
-Vue.use(VueRouter)
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
 // 注入拦截
 router.beforeEach(routerBeforeEach)
 
