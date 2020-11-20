f15064d0167dfbf12d62168c25b43d423d1f42c7
caohefei <hefei.cao@xiaoyangedu.com>
2020-11-20 14:55:52 +0800

    fix bug

--- default/src/main.js
+++ default/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import viewUI from './viewUI'
-
-const app = createApp(App).use(store).use(router).mount('#app')
-
+const app = createApp(App)
 viewUI(app)
+app.use(store).use(router).mount('#app')
